dab9a9fc28ebb06d376ddf3eb6b0c412586a5511
kevalshah <kevalshah93@gmail.com>
2014-10-12 22:42:17 -0700

    Added getPackage() to ClassAnalyzer and added unit tests for getPackage() and ClassAnalyzer constructor

--- Code Galaxy/src/analyzer/miscstaticanalyzer/ClassAnalyzer.java
+++ Code Galaxy/src/analyzer/miscstaticanalyzer/ClassAnalyzer.java
@@ -1,7 +1,34 @@
 package analyzer.miscstaticanalyzer;
 
+import japa.parser.JavaParser;
+import japa.parser.ParseException;
+import japa.parser.ast.CompilationUnit;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class ClassAnalyzer {
 
+    private CompilationUnit compilationUnit;
 
+    public ClassAnalyzer(File javaFile) throws IOException, ParseException {
+        if(!javaFile.exists()) {
+            throw new FileNotFoundException();
+        }
+
+        if(!javaFile.getName().contains(".java")) {
+            throw new IllegalArgumentException();
+        }
+
+        compilationUnit = JavaParser.parse(javaFile);
+    }
+
+    public String getPackage() {
+        String packageName = compilationUnit.getPackage().toString().trim();
+        packageName = packageName.replace("package ", "");
+        packageName = packageName.replace(";", "");
+        return packageName;
+    }
 
 }
---
+++ Code Galaxy/tests/analyzer/miscstaticanalyzer/ClassAnalyzerTest.java
@@ -0,0 +1,71 @@
+package analyzer.miscstaticanalyzer;
+
+import japa.parser.ParseException;
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class ClassAnalyzerTest extends TestCase {
+
+    private ClassAnalyzer classAnalyzer;
+
+    /**
+     * Tests constructor by passing in a non-existing file as an argument
+     * Expected Result: A FileNotFoundException should be thrown
+     *
+     * @throws Exception
+     */
+    public void testConstructorWithNonExistingFile() throws Exception {
+        try {
+            classAnalyzer = new ClassAnalyzer(new File("test_resources/codebase1/non_existing_file.java"));
+            fail();
+        } catch(FileNotFoundException e) {
+
+        }
+    }
+
+
+    /**
+     * Tests constructor with a Java file that has syntax errors as an argument
+     * Expected Result: A ParseException should be thrown
+     *
+     * @throws Exception
+     */
+    public void testConstructorWithSyntaxErrorJavaFile() throws Exception {
+        try {
+            classAnalyzer = new ClassAnalyzer(new File("test_resources/FileWithErrors.java"));
+            fail();
+        } catch(ParseException e) {
+
+        }
+    }
+
+
+    /**
+     * Tests constructor with a non-Java file as an argument
+     * Expected Result: An IllegalArgumentException should be thrown
+     *
+     * @throws Exception
+     */
+    public void testConstructorWithNonJavaFile() throws Exception {
+        try {
+            classAnalyzer = new ClassAnalyzer(new File("test_resources/Text.txt"));
+            fail();
+        } catch(IllegalArgumentException e) {
+
+        }
+    }
+
+
+    /**
+     * Tests getPackage() method to see if the correct package for a class is returned
+     * Expected Result: "test"
+     *
+     * @throws Exception
+     */
+    public void testGetPackage() throws Exception {
+        classAnalyzer = new ClassAnalyzer(new File("test_resources/Test.java"));
+        assertEquals("test", classAnalyzer.getPackage());
+    }
+}
