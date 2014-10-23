201a04eae24963e33ba2f0d2a333808c55c8a067
Keval Shah <kshah2451@gmail.com>
2014-10-23 11:49:49 -0700

    Added getMiscStaticMetrics() function to MiscStaticAnalyzer.java

--- Code Galaxy/src/analyzer/miscstaticanalyzer/MiscStaticAnalyzer.java
+++ Code Galaxy/src/analyzer/miscstaticanalyzer/MiscStaticAnalyzer.java
@@ -1,10 +1,13 @@
 package analyzer.miscstaticanalyzer;
 
+import japa.parser.ParseException;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -14,6 +17,7 @@ public class MiscStaticAnalyzer {
 
     /**
      * Gets all .java files from input parameter path
+     *
      * @param codebasePath - The path to the codebase
      * @return
      */
@@ -22,5 +26,47 @@ public class MiscStaticAnalyzer {
                 TrueFileFilter.TRUE);
     }
 
+    /**
+     * Gets static code analysis metrics for a code base
+     *
+     * @param codebasePath - The path to the codebase
+     * @return
+     */
+    public static MiscCodeBaseStats getMiscStaticMetrics(String codebasePath) {
+
+        // Get all Java source files from codebase
+        Collection<File> sourceFiles = getSourceFiles(codebasePath);
+
+        // Bundled object that holds all the source file statistics
+        MiscCodeBaseStats statistics = new MiscCodeBaseStats();
+
+        // Iterate through all source files
+        for (File sourceFile : sourceFiles) {
+            try {
+                ClassAnalyzer analyzer = new ClassAnalyzer(sourceFile);
+
+                // Get package name for a source file and add to MiscCodeBaseStats
+                String packageName = analyzer.getPackage();
+                statistics.addPackage(packageName);
+
+                // Get class(es) information from the class analyzer
+                ArrayList<ClassInfo> classInfos = analyzer.getClassInfo();
+
+                // Iterate through all classes (they may be nested classes) for a source file
+                for (ClassInfo classInfo : classInfos) {
+                    // Add class information to MiscCodeBaseStats
+                    statistics.addClass(classInfo);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Return bundled object with all static code analysis metrics for the code base
+        return statistics;
+    }
 
 }
