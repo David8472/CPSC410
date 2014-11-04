2ac1d7f10ed48d088a8881639fa2c76c449c9a91
David8472 <david_lu49@yahoo.ca>
2014-11-03 17:43:41 -0800

    Fixed some names.

--- CodeGalaxy/src/analyzer/gitcommitcomponent/CommitAnalyzerInfo.java
+++ CodeGalaxy/src/analyzer/gitcommitcomponent/CommitAnalyzerInfo.java
@@ -2,16 +2,16 @@ package analyzer.gitcommitcomponent;
 
 import java.util.ArrayList;
 
-public class CommitData {
+public class CommitAnalyzerInfo {
 	private String repoDirectory;
 	private int commitNumber;//Commit Number is the order of the commit, Ex.) Initial commit is 1
 	private ArrayList<String> filesAdded = new ArrayList<String>();
 	private ArrayList<String> filesChanged = new ArrayList<String>();
 	private ArrayList<String> filesDeleted = new ArrayList<String>();
-	private String authorNames;
+	private String authorName;
 	private String commitHashID;
 	
-	public CommitData(String repoDirectory, int commitNumber){//Constructor
+	public CommitAnalyzerInfo(String repoDirectory, int commitNumber){//Constructor
 		this.repoDirectory = repoDirectory;
 		this.commitNumber = commitNumber;
 	}
@@ -33,7 +33,7 @@ public class CommitData {
 	}
 	
 	public String getAuthorName(){
-		return authorNames;
+		return authorName;
 	}
 	
 	public String getCommitHashID(int commitNumber){
