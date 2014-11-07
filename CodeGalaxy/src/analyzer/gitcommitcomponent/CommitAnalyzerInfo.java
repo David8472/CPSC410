a229608d2d24a6df645c907b37619475cc4d1594
David8472 <david_lu49@yahoo.ca>
2014-11-06 18:49:50 -0800

    Fixed some naming errors.

--- CodeGalaxy/src/analyzer/gitcommitcomponent/CommitAnalyzerInfo.java
+++ CodeGalaxy/src/analyzer/gitcommitcomponent/CommitAnalyzerInfo.java
@@ -1,112 +1,89 @@
 package analyzer.gitcommitcomponent;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.lib.RepositoryBuilder;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevTree;
-import org.eclipse.jgit.treewalk.TreeWalk;
-
-public class CommitAnalyzer{
+//This is the data on a single commit
+public class CommitAnalyzerInfo {
 	private String repoDirectory;
-	private ArrayList<CommitAnalyzerInfo> commitAnalyzerInfo = new ArrayList<CommitAnalyzerInfo>();
+	private int commitNumber;//Commit Number is the order of the commit, Ex.) Initial commit is 0, Only Java commits counted
+	private ArrayList<String> filesAdded = new ArrayList<String>();
+	private ArrayList<String> allJavaFiles = new ArrayList<String>();
+	private ArrayList<String> filesDeleted = new ArrayList<String>();
+
+	private String authorName;
+	private String commitID;
 	
-	private int numberOfJavaCommits=0;
-	private int numberOfAllCommits=0;
-	
-	public CommitAnalyzer(String repoDirectory){//The Constructor
-		this.repoDirectory=repoDirectory;		
+	public CommitAnalyzerInfo(String repoDirectory){//Constructor for blank commitData
+		this.repoDirectory = repoDirectory;
 	}
 	
-	public ArrayList<CommitAnalyzerInfo> analyzeCommits(){//This will analyze the commits of the Repository
-		File directory = new File(repoDirectory);
-	    RepositoryBuilder builder = new RepositoryBuilder();
-	    Repository repository;
-	    
-		try {
-			repository = builder.setGitDir(directory).readEnvironment().findGitDir().build();
-			Git git = new Git(repository);
-		    RevCommit revCommit = null;
-		    
-		    if(repository.getRef("master") != null){
-			    Iterable<RevCommit> logs = git.log().call();
-			    
-			    while(logs.iterator().hasNext()) {
-			    	CommitAnalyzerInfo tempCommitAnalyzerInfo = new CommitAnalyzerInfo(repoDirectory);
-				    ArrayList<String> tempStringArray = new ArrayList<String>();
-			        revCommit = logs.iterator().next();
-			        RevTree tree = revCommit.getTree();
-			        TreeWalk treeWalk = new TreeWalk(repository);
-			        treeWalk.addTree(tree);
-			        treeWalk.setRecursive(true);
-			        
-			        if(numberOfAllCommits == 0){
-			        	commitAnalyzerInfo.add(new CommitAnalyzerInfo(repoDirectory));//First Commit, initial state of empty repository
-			        	commitAnalyzerInfo.get(0).setCommitNumber(0);
-			        	commitAnalyzerInfo.get(0).setAuthorName(revCommit.getAuthorIdent().getName());
-			        	commitAnalyzerInfo.get(0).setCommitID(revCommit.getId().getName());
-			        }
-			        
-			        numberOfAllCommits++;
-			        while (treeWalk.next()) {
-			        	if(treeWalk.getNameString().contains(".java")){//Will only bother with Java files
-			        		tempStringArray.add(new String(treeWalk.getNameString()));
-			        		
-			        	}
-			        }
-			        
-			        
-			        if(tempStringArray.size() > 0){
-			        	tempCommitAnalyzerInfo.setAllJavaFiles(new ArrayList<String>(tempStringArray));
-			        	tempStringArray.clear();
-			        	
-			        	tempCommitAnalyzerInfo.setAuthorName(revCommit.getAuthorIdent().getName());
-			        	tempCommitAnalyzerInfo.setCommitID(revCommit.getId().getName());
-			        	
-			        	//Adds all files in current commit, removes the files in previous commit, ends up with list of files added
-			        	tempStringArray.addAll(tempCommitAnalyzerInfo.getAllJavaFiles());
-			        	tempStringArray.removeAll(commitAnalyzerInfo.get(numberOfJavaCommits).getAllJavaFiles());
-			        	tempCommitAnalyzerInfo.setFilesAdded(new ArrayList<String>(tempStringArray));
-			        	tempStringArray.clear();
-			        	
-			        	//Adds all files of previous commit, removes files in current commit, ends up with list of files deleted
-			        	tempStringArray.addAll(commitAnalyzerInfo.get(numberOfJavaCommits).getAllJavaFiles());
-			        	tempStringArray.removeAll(tempCommitAnalyzerInfo.getAllJavaFiles());
-			        	tempCommitAnalyzerInfo.setFilesDeleted(new ArrayList<String>(tempStringArray));
-			        	tempStringArray.clear();
-			        	
-			        	numberOfJavaCommits++;
-			        	tempCommitAnalyzerInfo.setCommitNumber(numberOfJavaCommits);
-			        	commitAnalyzerInfo.add(new CommitAnalyzerInfo(tempCommitAnalyzerInfo));
-			        }
-			        tempCommitAnalyzerInfo.clear();
-			        tempStringArray.clear();
-			        treeWalk.reset();
-			    }
-		    }
-		    else{
-		    	System.out.println("Invalid Repository at "+repoDirectory);
-		    }
-		    repository.close();
-		    git.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (GitAPIException e) {
-			e.printStackTrace();
-		}
-		return commitAnalyzerInfo;
+	public CommitAnalyzerInfo(CommitAnalyzerInfo commitData) {//Constructor for copying in data
+		this.repoDirectory = commitData.repoDirectory;
+		this.setCommitNumber(commitData.getCommitNumber());
+		this.filesAdded.addAll(commitData.getFilesAdded());
+		this.allJavaFiles.addAll(commitData.getAllJavaFiles());
+		this.filesDeleted.addAll(commitData.getFilesDeleted());
+		this.authorName  = commitData.getAuthorName();
+		this.commitID = commitData.getCommitID();
+	}
+
+	public String getRepoDirectory(){
+		return repoDirectory;
 	}
 	
-	public int getNumberOfJavaCommits() {
-		return numberOfJavaCommits;
+	public int getCommitNumber(){
+		return commitNumber;
 	}
 	
-	public int getNumberOfAllCommits() {
-		return numberOfAllCommits;
+	protected void setCommitNumber(int commitNumber){
+		this.commitNumber = commitNumber;
 	}
+	
+	public ArrayList<String> getFilesAdded(){
+		return filesAdded;
+	}
+	
+	protected void setFilesAdded(ArrayList<String> filesAdded){
+		this.filesAdded.addAll(filesAdded);
+	}
+	
+	public ArrayList<String> getAllJavaFiles(){
+		return allJavaFiles;
+	}
+	
+	protected void setAllJavaFiles(ArrayList<String> allJavaFiles){
+		this.allJavaFiles.addAll(allJavaFiles);
+	}
+	
+	public ArrayList<String> getFilesDeleted(){
+		return filesDeleted;
+	}
+	
+	protected void setFilesDeleted(ArrayList<String> filesDeleted){
+		this.filesDeleted.addAll(filesDeleted);
+	}
+	
+	public String getAuthorName(){
+		return authorName;
+	}
+	
+	protected void setAuthorName(String authorName){
+		this.authorName = authorName;
+	}
+	
+	public String getCommitID(){
+		return commitID;
+	}
+	
+	protected void setCommitID(String commitID){
+		this.commitID = commitID;
+	}
+	
+	protected void clear(){
+		commitNumber = 0;
+		filesAdded.clear();
+		allJavaFiles.clear();
+		filesDeleted.clear();
+	}
+	
 }
