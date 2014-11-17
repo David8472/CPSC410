f15c326cef78c84adc77f608b9eb9c7485b7770a
David8472 <david_lu49@yahoo.ca>
2014-11-16 19:07:36 -0800

    Fixed path for DataAggregator.

--- CodeGalaxy/src/main/GlobalController.java
+++ CodeGalaxy/src/main/GlobalController.java
@@ -25,7 +25,7 @@ public class GlobalController {
     	ArrayList<CommitAnalyzerInfo> commitMetaData = new ArrayList<CommitAnalyzerInfo>();
     	commitMetaData = commitAnalyzer.analyzeCommits();
     	
-    	DataAggregator dataAggregator = new DataAggregator(repoDirectory+"\\.git");
+    	DataAggregator dataAggregator = new DataAggregator("CodeGalaxy");
     	
     	HashMap<String, Integer> previousCommit = new HashMap<String, Integer>();//For tracking information
     	
